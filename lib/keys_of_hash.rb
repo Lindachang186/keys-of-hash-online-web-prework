86bad3bd3b2d5afadb17e85325aaf68249638e17
Linda Chang <lindachang186@gmail.com>
2018-10-25 21:37:53 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,14 @@
+#  let(:animals) {
+#{"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
+
+
+
 class Hash
   def keys_of(*arguments)
     animals = []
     animals << arguments
       self.each do |key, value|
-        puts value
-    #end
+        puts key
     end
   end
 end
