24d14ed27f450f4ee85214fe4c085e889d997147
Linda Chang <lindachang186@gmail.com>
2018-10-25 21:55:12 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 #  let(:animals) {
 #{"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
 
-
+require "pry"
 
 class Hash
   def keys_of(*arguments)
